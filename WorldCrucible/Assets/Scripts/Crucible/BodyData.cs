fbff2677851145bcfec08336e1be4471f615a948
Raquel F <noodlesthebird@gmail.com>
2025-11-09 02:06:43 -0600

    Some planet scriptobjs

--- WorldCrucible/Assets/Scripts/Crucible/BodyData.cs
+++ WorldCrucible/Assets/Scripts/Crucible/BodyData.cs
@@ -1,7 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "BodyData", menuName = "Scriptable Objects/BodyData")]
+[CreateAssetMenu(menuName = "CrucibleLab/CelestialBody", fileName = "NewCelestialBody")]
 public class BodyData : ScriptableObject
 {
-    
+    public string bodyName;
+    public Sprite bodySprite;
+    public List<ElementRatio> composition = new();
+    public float temperature; // Kelvin
+    public float radius;
+    public float mass; // In terms of Earth Mass (1)
+
+    [System.Serializable]
+    public struct ElementRatio
+    {
+        public ElementData element;
+        [Range(0f, 1f)]
+        public float ratio;
+    }
 }

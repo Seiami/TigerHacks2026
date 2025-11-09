3759af8e5a5920939cc864be8d71769e5d31fe1f
Raquel F <noodlesthebird@gmail.com>
2025-11-08 18:01:39 -0600

    More stuff

--- WorldCrucible/Assets/Scripts/Crucible/ElementIcon.cs
+++ WorldCrucible/Assets/Scripts/Crucible/ElementIcon.cs
@@ -1,20 +1,61 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-/// <summary>
-/// Stores element data: name, atomic weight, category, etc
-/// </summary>
-public class ElementIcon : MonoBehaviour
+[ExecuteAlways]
+[RequireComponent(typeof(Image))]
+[RequireComponent(typeof(CanvasGroup))]
+public class ElementIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Element Info")]
+    public ElementData elementData; // assign in inspector
+
+    private Image image;
+    private RectTransform rectTransform;
+    private Canvas canvas;
+    private CanvasGroup canvasGroup;
+    private Transform originalParent;
+
+    private void Awake()
     {
-        
+        image = GetComponent<Image>();
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvas = GetComponentInParent<Canvas>();
+
+        if (elementData != null)
+            UpdateIconVisual();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateIconVisual()
     {
+        // Set the sprite and optionally a color tint
+        image.sprite = elementData.icon;
+        image.preserveAspect = true;
+    }
 
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalParent = transform.parent;
+        transform.SetParent(canvas.transform, true); // move to root canvas for smooth dragging
+        canvasGroup.blocksRaycasts = false; // allow drop zones to detect this
+        canvasGroup.alpha = 0.8f; // slightly transparent for feedback
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+
+        // If dropped nowhere (no new parent assigned), return to original
+        if (transform.parent == canvas.transform)
+        {
+            transform.SetParent(originalParent, false);
+        }
     }
 }
